f413a02a638f0a688eec60aeb6497fd3c09348e8
alisha musamabik <alisha.musamabik@gmail.com>
2020-09-29 18:40:36 +0100

    add routes - static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'commercial',  to: 'pages#commercial'
+  get'residential',  to: 'pages#residential'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
